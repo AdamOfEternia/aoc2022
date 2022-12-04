401f526bbab43b85fb8594f2605beef1debaa66f
Damian Lord <damian.lord@bjss.com>
2022-12-04 15:48:26 +0000

    Day 04 - Pt1

---
+++ day04/day04.py
@@ -0,0 +1,57 @@
+import names
+from dataclasses import dataclass
+
+
+@dataclass
+class Elf:
+    name: str
+    section_start: int
+    section_end: int
+
+    def __init__(self, section_start, section_end):
+        self.name = names.get_first_name()
+        self.section_start = section_start
+        self.section_end = section_end
+
+
+@dataclass
+class Team:
+    elf1: Elf
+    elf2: Elf
+
+    def __init__(self, elf1, elf2):
+        self.elf1 = elf1
+        self.elf2 = elf2
+
+    def do_elves_clash_fully(self):
+        if self.elf1.section_start >= self.elf2.section_start and self.elf1.section_end <= self.elf2.section_end:
+            return True
+        elif self.elf2.section_start >= self.elf1.section_start and self.elf2.section_end <= self.elf1.section_end:
+            return True
+        return False
+
+
+def read_file():
+    with open("pairs.dat") as file:
+        lines = [line.rstrip() for line in file]
+    return lines
+
+
+def main():
+    teams = []
+    lines = read_file()
+    for line in lines:
+        first, second = line.split(',', 1)
+        start1, end1 = first.split('-', 1)
+        start2, end2 = second.split('-', 1)
+        teams.append(Team(elf1=Elf(int(start1), int(end1)), elf2=Elf(int(start2), int(end2))))
+
+    clash_count = 0
+    for team in teams:
+        if team.do_elves_clash_fully():
+            clash_count += 1
+    print(f"Number of clashing teams is {clash_count}")
+
+
+if __name__ == "__main__":
+    main()
