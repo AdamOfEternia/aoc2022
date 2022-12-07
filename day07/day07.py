6a815bb8b16101f53707d3847537eec7a2d74a91
Damian Lord <damian.lord@bjss.com>
2022-12-07 18:13:31 +0000

    Day 07 - Pt2

--- day07/day07.py
+++ day07/day07.py
@@ -75,6 +75,26 @@ def main():
             total_size += size
     print(total_size)
 
+    total_disk_space = 70000000
+    update_space_needed = 30000000
+    total_used_space = get_family_size(dirs=dirs, parent_dir=dirs[0])
+    free_space = total_disk_space - total_used_space
+    print(f"Disk={total_disk_space}, "
+          f"Used={total_used_space}, "
+          f"Free={free_space}, "
+          f"Required={update_space_needed}, "
+          f"Needed={(update_space_needed - free_space)}")
+
+    dir_to_delete = None
+    size_to_delete = total_disk_space
+    for d in dirs:
+        size = get_family_size(dirs=dirs, parent_dir=d)
+        if size >= (update_space_needed - free_space):
+            if size < size_to_delete:
+                size_to_delete = size
+                dir_to_delete = d
+    print(f"Delete {dir_to_delete} to free up {size_to_delete} space")
+
 
 if __name__ == "__main__":
     main()
