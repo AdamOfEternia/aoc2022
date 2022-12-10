ce126806c4bbb1d7ddf3599c1b157545228b9f1c
Damian Lord <damian.lord@bjss.com>
2022-12-10 14:57:12 +0000

    Day 10 - Pt2

--- day10/day10.py
+++ day10/day10.py
@@ -22,10 +22,24 @@ def main():
                 cycle += 1
                 reg_x += int(rest[0])
 
+    # pt1
     signal_strength = 0
     for cycle in range(19, len(cycles), 40):
         signal_strength += (cycle + 1) * cycles[cycle][1]
     print(f"Signal strength={signal_strength}")
+
+    # pt2
+    col = 0
+    for cycle in cycles:
+        sprite_pos = cycle[1]
+        if (sprite_pos-1) <= col <= (sprite_pos+1):
+            print("#", end='')
+        else:
+            print(".", end='')
+        col += 1
+        if col > 39:
+            print()
+            col = 0
 
 
 if __name__ == "__main__":
